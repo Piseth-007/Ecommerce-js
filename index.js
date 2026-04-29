e879919d9ea20763df88dfea4334c16682fb23ff
LeangPiseth <pisethleang6@gmail.com>
2026-04-29 23:08:05 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+let Product_array = [];
+let cart = [];
+
+fetch(
+  "https://thoenthonny.github.io/data-json-computer/?fbclid=IwY2xjawRe0G1leHRuA2FlbQIxMABicmlkETFTc0dicTM2MTI5VFV6cVJqc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHtoOmF1hwBFdoK9WyNLhd-hqUKaGtjCVei2MzQ5gA3k9Cu6ILLEuG5HrtAxx_aem_moNiMGXFtdAPq0PdtBd5Fg",
+)
+  .then((res) => res.json())
+  .then((product) => {
+    Product_array = product;
+    Display(Product_array);
+  })
+  .catch((err) => console.log(err));
+const Display = (prd) => {
+  const container = document.getElementById("show-product");
+  container.innerHTML = "";
+
+  prd.slice(0, 8).forEach((element, index) => {
+    container.innerHTML += `
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="card h-100 border-0 product-card">
+
+          <img src="${element.image}" class="card-img-top" alt=".." />
+
+          <div class="card-body d-flex flex-column">
+
+            <h5 class="card-title">${element.name}</h5>
+
+            <p class="card-text text-muted small">
+              ${element.discription}
+            </p>
+
+            <h6 class="text-primary mb-3">$${element.price}</h6>
+
+            <button onclick="AddtoCard(${index})"
+              class="btn btn-primary mt-auto w-100">
+
+              <i class="bi bi-cart-plus"></i> Add To Cart
+            </button>
+
+          </div>
+        </div>
+      </div>
+    `;
+  });
+};
+const AddtoCard = (index) => {
+  const product = Product_array[index];
+
+  cart.push(product);
+
+  console.log(cart);
+
+  document.getElementById("cart_count").innerText = cart.length;
+};
